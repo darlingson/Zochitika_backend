ec01501ea6a827da4260dd0525d69bdfc96edd35
darlingson <dmakuwila@gmail.com>
2023-12-18 03:52:04 +0200

    added code to search, filter and select generated by chatGPT

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 from datetime import timedelta
 
 from flask_mysqldb import MySQL
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 app.config['MYSQL_USER'] = 'root'
@@ -34,7 +33,55 @@ def getZochitikaAll():
         json_data.append(row_data)
     return jsonify(json_data)
     # return str(rv)
+@app.route("/zochitika/<id>")
+def getZochitikaById(id):
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM zochitika WHERE id = %s", (id,))
+    rv = cur.fetchall()
+    json_data = []
+    row_headers = [x[0] for x in cur.description] #extracting row headers
+    for result in rv:
+        row_data = dict(zip(row_headers,result))
+        for key, value in row_data.items():
+            if isinstance(value, timedelta):
+                # convert timedelta to string
+                row_data[key] = str(value)
+        json_data.append(row_data)
+    return jsonify(json_data)
 
+@app.route("/zochitika/search")
+def searchZochitika():
+    keyword = request.args.get('keyword')
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM zochitika WHERE your_field LIKE %s", ('%' + keyword + '%',))
+    rv = cur.fetchall()
+    json_data = []
+    row_headers = [x[0] for x in cur.description] #extracting row headers
+    for result in rv:
+        row_data = dict(zip(row_headers,result))
+        for key, value in row_data.items():
+            if isinstance(value, timedelta):
+                # convert timedelta to string
+                row_data[key] = str(value)
+        json_data.append(row_data)
+    return jsonify(json_data)
+
+@app.route("/zochitika/filter")
+def filterZochitika():
+    date = request.args.get('date')
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM zochitika WHERE your_date_field = %s", (date,))
+    rv = cur.fetchall()
+    json_data = []
+    row_headers = [x[0] for x in cur.description] #extracting row headers
+    for result in rv:
+        row_data = dict(zip(row_headers,result))
+        for key, value in row_data.items():
+            if isinstance(value, timedelta):
+                # convert timedelta to string
+                row_data[key] = str(value)
+        json_data.append(row_data)
+    return jsonify(json_data)
 
 if __name__ == '__main__':
     app.run(debug=True,port=8085)
