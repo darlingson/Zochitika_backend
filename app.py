a8a2bb207b1e0ba9e3889003ae3c349854df310e
darlingson <dmakuwila@gmail.com>
2023-12-18 03:46:12 +0200

    added code to return json, but also to check for timedelta

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+# from flask import Flask
+from datetime import timedelta
+
 from flask_mysqldb import MySQL
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 app.config['MYSQL_USER'] = 'root'
@@ -19,8 +23,17 @@ def getZochitikaAll():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM zochitika")
     rv = cur.fetchall()
-
-    return str(rv)
+    json_data = []
+    row_headers = [x[0] for x in cur.description] #extracting row headers
+    for result in rv:
+        row_data = dict(zip(row_headers,result))
+        for key, value in row_data.items():
+            if isinstance(value, timedelta):
+                # convert timedelta to string
+                row_data[key] = str(value)
+        json_data.append(row_data)
+    return jsonify(json_data)
+    # return str(rv)
 
 
 if __name__ == '__main__':
--- sb.sql
+++ sb.sql
@@ -18,3 +18,5 @@ ALTER TABLE `zochitika`
  ADD `end_date` DATE NULL DEFAULT NULL AFTER `date`,
  ADD `time` TIME NULL DEFAULT NULL AFTER `end_date`,
  ADD `end_time` TIME NULL DEFAULT NULL AFTER `time`;
+
+INSERT INTO `zochitika` (`title`, `organiser`, `date`, `end_date`, `time`, `end_time`, `description`, `location`, `type`, `entry_fee`, `address`, `coordinates`, `id`) VALUES ('Soirée de Luxe', 'The Sunday Soiree', '2023-12-16', '2023-12-17', '12:00:00', '12:30:00', 'As the year draws to a close, join us for an extraordinary end-of-year soirée event. Soirée de Luxe doubles as the perfect homecoming celebration.', 'Lassie House Zomba Street, Area 10, House No. 10/85, Lilongwe', 'Event', '20,000', 'Zomba Street, Area 10, House No. 10/85 Lilongwe', '-13.938678652825335, 33.804071176631645', NULL)
