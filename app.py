409ed19f7675940aa5bb061617573612aa19c1f0
darlingson <dmakuwila@gmail.com>
2023-12-18 03:26:18 +0200

    added code to retrieve all zochitika from the database

--- app.py
+++ app.py
@@ -8,11 +8,20 @@ app.config['MYSQL_DB'] = 'zochitika_db'
 app.config['MYSQL_HOST'] = 'localhost'
 
 mysql = MySQL(app)
-app = Flask(__name__)
+# app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello, World!"
+
+@app.route("/zochitika")
+def getZochitikaAll():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM zochitika")
+    rv = cur.fetchall()
+
+    return str(rv)
+
 
 if __name__ == '__main__':
     app.run(debug=True,port=8085)
