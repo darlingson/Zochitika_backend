a5d10880668c239415e9f6c5999e69d5e518cfd7
darlingson <dmakuwila@gmail.com>
2023-12-22 08:12:23 +0200

    added a function to connect on every request

--- app.py
+++ app.py
@@ -1,25 +1,58 @@
-# from flask import Flask
-from datetime import timedelta
 
-from flask_mysqldb import MySQL
-from flask import Flask, jsonify, request
+# from flask import Flask
+from flask_bootstrap import Bootstrap
+# from flask import jsonify
+import json
+from datetime import timedelta
+from flask import Flask, jsonify, request,render_template
+import MySQLdb as my
 
 app = Flask(__name__)
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'zochitika_db'
+bootstrap = Bootstrap(app)
 app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] =  'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] =  'zochitika_db'
+# mysql = MySQL(app)
+# db = MySQLdb.connect('Darlingson.mysql.pythonanywhere-services.com', 'username-of-db', 'password-of-db', 'database-name')
 
-mysql = MySQL(app)
-# app = Flask(__name__)
 
+db = my.connect ('Darlingson.mysql.pythonanywhere-services.com','Darlingson','shinobi4313','Darlingson$default')
+db.ping(True)
+cursor = db.cursor ()
+
+def connectDB():
+    db = my.connect('Darlingson.mysql.pythonanywhere-services.com', 'Darlingson', 'shinobi4313', 'Darlingson$default')
+    db.ping(True)
+    # cursor = db.cursor()]
+    return db
 @app.route('/')
-def home():
-    return "Hello, World!"
+def hello_world():
+    return render_template('index.html')
+@app.route('/analytics')
+def analytics():
+    return render_template('analyticsApp.html')
+@app.route('/compare_team',methods = ['GET'])
+def compare_team():
+    return render_template('compare_team.html')
+@app.route('/compare_players',methods =['GET'])
+def compare_players():
+    return render_template('compare_players.html')
+@app.route('/teams',methods=['GET'])
+def getteams():
+    data = json.load(open('/home/Darlingson/mysite/2023superLeagueteams.json'))
+    return jsonify(data), 200, {"Access-Control-Allow-Origin": "*"}
+@app.route('/stats')
+def stats():
+    data = json.load(open('/home/Darlingson/mysite/csvjson.json'))
+    return jsonify(data), 200, {"Access-Control-Allow-Origin": "*"}
+    #return jsonify(data)
 
 @app.route("/zochitika")
 def getZochitikaAll():
-    cur = mysql.connection.cursor()
+    # cur = mysql.connection.cursor()
+    con = connectDB()
+    cur = con.cursor()
     cur.execute("SELECT * FROM zochitika")
     rv = cur.fetchall()
     json_data = []
@@ -35,7 +68,8 @@ def getZochitikaAll():
     # return str(rv)
 @app.route("/zochitika/<id>")
 def getZochitikaById(id):
-    cur = mysql.connection.cursor()
+    # cur = mysql.connection.cursor()
+    cur = db.cursor ()
     cur.execute("SELECT * FROM zochitika WHERE id = %s", (id,))
     rv = cur.fetchall()
     json_data = []
@@ -52,8 +86,9 @@ def getZochitikaById(id):
 @app.route("/zochitika/search")
 def searchZochitika():
     keyword = request.args.get('keyword')
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM zochitika WHERE title LIKE %s OR description LIKE %s",('%' + keyword + '%', '%' + keyword + '%',))
+    # cur = mysql.connection.cursor()
+    cur = db.cursor ()
+    cur.execute("SELEC/T * FROM zochitika WHERE title LIKE %s OR description LIKE %s",('%' + keyword + '%', '%' + keyword + '%',))
     rv = cur.fetchall()
     json_data = []
     row_headers = [x[0] for x in cur.description] #extracting row headers
@@ -69,7 +104,8 @@ def searchZochitika():
 @app.route("/zochitika/filter")
 def filterZochitika():
     date = request.args.get('date')
-    cur = mysql.connection.cursor()
+    # cur = mysql.connection.cursor()
+    cur = db.cursor ()
     cur.execute("SELECT * FROM zochitika WHERE date = %s", (date,))
     rv = cur.fetchall()
     json_data = []
@@ -82,6 +118,3 @@ def filterZochitika():
                 row_data[key] = str(value)
         json_data.append(row_data)
     return jsonify(json_data)
-
-if __name__ == '__main__':
-    app.run(debug=True,port=8085)
